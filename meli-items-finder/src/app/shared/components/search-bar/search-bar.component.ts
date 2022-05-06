baea3ff103e0851d662221cf03c899800ee79afb
Camila <cami.aristi95@gmail.com>
2022-05-05 21:31:16 -0500

    creates search bar component

--- meli-items-finder/src/app/shared/components/search-bar/search-bar.component.ts
+++ meli-items-finder/src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
+  selector: 'search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.sass']
 })
 export class SearchBarComponent implements OnInit {
 
+  query = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  test(){
+    console.log('query', this.query)
   }
 
 }
--- meli-items-finder/src/app/shared/shared.module.ts
+++ meli-items-finder/src/app/shared/shared.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+export const SHARED_MODULES: any[] = [
+  CommonModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  FormsModule
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    ...SHARED_MODULES
+  ],
+  exports: [
+    ...SHARED_MODULES
   ]
 })
 export class SharedModule { }
