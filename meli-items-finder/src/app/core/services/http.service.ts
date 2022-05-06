158210184b7c95b01bb4abde64e5eae949cc2c19
Camila <cami.aristi95@gmail.com>
2022-05-06 10:04:40 -0500

    adds http service

---
+++ meli-items-finder/src/app/core/services/http.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { RequestModel } from '../models/request.model'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getRequest(data: RequestModel): Observable<Object>{
+    return this.http.get(`http://api/${data.params}`)
+  }
+
+  postRequest(data: RequestModel): Observable<Object>{
+    return this.http.get(`http://api/${data.params}`, data.payload)
+  }
+
+  putRequest(data: RequestModel): Observable<Object>{
+    return this.http.put(`http://api/${data.params}`, data.payload)
+  }
+
+  deleteRequest(data: RequestModel): Observable<Object>{
+    return this.http.delete(`http://api/${data.params}`)
+  }
+
+}
